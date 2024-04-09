16d9ba8349ec575459695359a786166c062c79bf
facubotto13 <151096391+facubotto13@users.noreply.github.com>
2024-04-09 17:12:23 -0300

    Primer Commit
    Primer Commit donde pegamos los ejercicios de la clase pasada

---
+++ Ejercicio1-Pseudocodigo/Ejercicio1-Pseucodigo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*CALCULAR LA SUMA DE 2 NUMEROS*/
+
+Console.Write("Ingrese un número: ");
+double numero1, numero2; /*pongo double para que permita numeros con decimales*/
+if (!Double.TryParse(Console.ReadLine(), out numero1)) /*pongo Double.TryParse para que valide y convierta los numeros ingresados*/
+{
+    Console.WriteLine("Error, debe ingresar un número correcto");
+}
+else
+{
+    Console.Write("Ingrese otro número: ");
+    if (!Double.TryParse(Console.ReadLine(), out numero2))
+    {
+        Console.WriteLine("Error, debe ingresar un número correcto");
+    }
+    else
+    {
+        Console.WriteLine("La suma de los números es: " + (numero1 + numero2));
+    }
+}
